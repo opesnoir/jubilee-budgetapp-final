dc499109f1e270e386d18f71e51eea7a9c1fcfa7
opesnoir <goodsurroundsme@outlook.com>
2023-07-20 15:42:58 +0200

    feat model account

--- src/main/java/com/example/jubileebudgetapp/models/Account.java
+++ src/main/java/com/example/jubileebudgetapp/models/Account.java
@@ -1,4 +1,29 @@
 package com.example.jubileebudgetapp.models;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@NoArgsConstructor
+@AllArgsConstructor
+
+@Getter
+@Setter
+
+@Entity
+@Table(name = "account")
 public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+    private LocalDate dateCreated;
+    private BigDecimal balance;
+
 }
