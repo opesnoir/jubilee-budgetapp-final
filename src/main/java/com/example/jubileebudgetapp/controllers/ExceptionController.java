399d4e1448dd5231c7f31dbcb78f18b5d91901ab
opesnoir <goodsurroundsme@outlook.com>
2023-07-20 20:36:40 +0200

    feat exceptioncontroller

---
+++ src/main/java/com/example/jubileebudgetapp/controllers/ExceptionController.java
@@ -0,0 +1,48 @@
+package com.example.jubileebudgetapp.controllers;
+
+
+import com.example.jubileebudgetapp.exceptions.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ExceptionController {
+
+    @ExceptionHandler(value = BadRequestException.class)
+    public ResponseEntity<String> exception(BadRequestException exception){
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(value = ContractNotFoundException.class)
+    public ResponseEntity<String> exception(ContractNotFoundException exception){
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.NO_CONTENT);
+    }
+
+    @ExceptionHandler(value = RecordNotFoundException.class)
+    public ResponseEntity<String> exception(RecordNotFoundException exception){
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(value = SavingGoalNotFoundException.class)
+    public ResponseEntity<String> exception(SavingGoalNotFoundException exception){
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(value = TransactionNotFoundException.class)
+    public ResponseEntity<String> exception(TransactionNotFoundException exception){
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.NO_CONTENT);
+    }
+
+    @ExceptionHandler(value = UploadFileNotFoundException.class)
+    public ResponseEntity<String> exception(UploadFileNotFoundException exception){
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(value = UsernameNotFoundException.class)
+    public ResponseEntity<String> exception(UsernameNotFoundException exception){
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+}
