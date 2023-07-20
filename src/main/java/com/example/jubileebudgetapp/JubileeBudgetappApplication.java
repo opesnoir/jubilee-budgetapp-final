f43dc4eb16a66199d50bfb8f8f9276448f87d4fb
opesnoir <goodsurroundsme@outlook.com>
2023-07-20 07:22:38 +0200

    style added jubilee-logo

---
+++ src/main/java/com/example/jubileebudgetapp/JubileeBudgetappApplication.java
@@ -0,0 +1,13 @@
+package com.example.jubileebudgetapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JubileeBudgetappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JubileeBudgetappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jubileebudgetapp/JubileeBudgetappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jubileebudgetapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JubileeBudgetappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
