ee46979ffd04b3a0bf931f318ed78fc99a9ab8f3
opesnoir <goodsurroundsme@outlook.com>
2023-07-20 15:33:07 +0200

    feat model contract

--- src/main/java/com/example/jubileebudgetapp/models/Contract.java
+++ src/main/java/com/example/jubileebudgetapp/models/Contract.java
@@ -1,4 +1,31 @@
 package com.example.jubileebudgetapp.models;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@NoArgsConstructor
+@AllArgsConstructor
+
+@Getter
+@Setter
+
+@Entity
+@Table(name = "contract")
 public class Contract {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String payee;
+    private String type;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private BigDecimal amount;
+
 }
