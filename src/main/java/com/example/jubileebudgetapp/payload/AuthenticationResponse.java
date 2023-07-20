d7951aecf619496edad09931ae6b8a2628294763
opesnoir <goodsurroundsme@outlook.com>
2023-07-20 20:43:36 +0200

    feat payload

---
+++ src/main/java/com/example/jubileebudgetapp/payload/AuthenticationResponse.java
@@ -0,0 +1,13 @@
+package com.example.jubileebudgetapp.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+
+public class AuthenticationResponse {
+
+    private final String jwt;
+
+}
