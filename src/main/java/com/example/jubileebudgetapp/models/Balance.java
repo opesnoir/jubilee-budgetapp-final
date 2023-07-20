de3aa1b38598816b88ac2ce0e71a9cdfc980bba6
opesnoir <goodsurroundsme@outlook.com>
2023-07-20 15:38:32 +0200

    feat model balance

--- src/main/java/com/example/jubileebudgetapp/models/Balance.java
+++ src/main/java/com/example/jubileebudgetapp/models/Balance.java
@@ -1,4 +1,30 @@
 package com.example.jubileebudgetapp.models;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@NoArgsConstructor
+@AllArgsConstructor
+
+@Getter
+@Setter
+
+@Entity
+@Table(name = "balance")
 public class Balance {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private BigDecimal totalExpense;
+    private BigDecimal totalIncome;
+    private BigDecimal balance;
+
+    // TODO: Relaties invoegen
+
 }
