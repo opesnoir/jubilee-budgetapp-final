65c4432cc243e6561a5372baa7e7340457bd288b
opesnoir <goodsurroundsme@outlook.com>
2023-07-21 11:19:04 +0200

    feat uploaddto

---
+++ src/main/java/com/example/jubileebudgetapp/dtos/UploadDto.java
@@ -0,0 +1,30 @@
+package com.example.jubileebudgetapp.dtos;
+
+import com.example.jubileebudgetapp.models.Account;
+import jakarta.persistence.Column;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+
+@Getter
+@Setter
+public class UploadDto {
+    private Long id;
+    private String fileName;
+
+    @Lob
+    private byte[] upload;
+
+    @Valid
+    private Account account;
+    private Long accountId;
+}
