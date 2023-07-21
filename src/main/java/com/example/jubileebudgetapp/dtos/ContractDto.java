3f4c944c98e09f8742a28801446a6be588ec1bb9
opesnoir <goodsurroundsme@outlook.com>
2023-07-21 10:58:15 +0200

    feat contractdto

---
+++ src/main/java/com/example/jubileebudgetapp/dtos/ContractDto.java
@@ -0,0 +1,37 @@
+package com.example.jubileebudgetapp.dtos;
+
+import com.example.jubileebudgetapp.models.Account;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Getter
+@Setter
+
+public class ContractDto {
+
+    private Long id;
+    @NotNull(message = "Payee cannot be null")
+    private String payee;
+    private String type;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    @NotNull(message = "Contract amount cannot be null")
+    private BigDecimal amount;
+
+    @Valid
+    private Account account;
+
+}
