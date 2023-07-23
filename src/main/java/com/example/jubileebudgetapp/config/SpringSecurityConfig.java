f90c46b7bcc5108f1b6708085d956da425b0815e
opesnoir <goodsurroundsme@outlook.com>
2023-07-23 12:22:36 +0200

    feat filter JwtRequestFilter

--- src/main/java/com/example/jubileebudgetapp/config/SpringSecurityConfig.java
+++ src/main/java/com/example/jubileebudgetapp/config/SpringSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.jubileebudgetapp.config;
 
+import com.example.jubileebudgetapp.filter.JwtRequestFilter;
 import com.example.jubileebudgetapp.services.CustomUserDetailsService;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -11,5 +12,8 @@ public class SpringSecurityConfig {
     private final CustomUserDetailsService customUserDetailsService;
     private final JwtRequestFilter jwtRequestFilter;
 
-
+    public SpringSecurityConfig(CustomUserDetailsService customUserDetailsService, JwtRequestFilter jwtRequestFilter) {
+        this.customUserDetailsService = customUserDetailsService;
+        this.jwtRequestFilter = jwtRequestFilter;
+    }
 }
