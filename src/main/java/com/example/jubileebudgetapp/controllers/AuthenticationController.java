402bbf6567278474d854f866d269473f39a4b665
opesnoir <goodsurroundsme@outlook.com>
2023-07-24 15:37:25 +0200

    feat AuthenticationController

---
+++ src/main/java/com/example/jubileebudgetapp/controllers/AuthenticationController.java
@@ -0,0 +1,59 @@
+package com.example.jubileebudgetapp.controllers;
+
+import com.example.jubileebudgetapp.payload.AuthenticationRequest;
+import com.example.jubileebudgetapp.payload.AuthenticationResponse;
+import com.example.jubileebudgetapp.services.CustomUserDetailsService;
+import com.example.jubileebudgetapp.utils.JwtUtil;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
+
+@CrossOrigin
+@RestController
+public class AuthenticationController {
+
+    private final AuthenticationManager authenticationManager;
+
+    private final CustomUserDetailsService customUserDetailsService;
+
+    private final JwtUtil jwtUtil;
+
+    public AuthenticationController(AuthenticationManager authenticationManager, CustomUserDetailsService customUserDetailsService, JwtUtil jwtUtil) {
+        this.authenticationManager = authenticationManager;
+        this.customUserDetailsService = customUserDetailsService;
+        this.jwtUtil = jwtUtil;
+    }
+
+    // returns principal (basic user data) of logged-in user
+    @GetMapping(value = "/authenticated")
+    public ResponseEntity<Object> authenticated(Authentication authentication, Principal principal) {
+        return ResponseEntity.ok().body(principal);
+    }
+
+    // return jwt-token when user enters the correct credentials.
+    @PostMapping(value = "/authenticate")
+    public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest) throws Exception {
+
+        String username = authenticationRequest.getUsername();
+        String password = authenticationRequest.getPassword();
+
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(username, password)
+            );
+        }
+        catch (BadCredentialsException ex) {
+            throw new Exception("Incorrect username or password", ex);
+        }
+
+        final UserDetails userDetails = customUserDetailsService.loadUserByUsername(username);
+        final String jwt = jwtUtil.generateToken(userDetails);
+        return ResponseEntity.ok(new AuthenticationResponse(jwt));
+    }
+}
