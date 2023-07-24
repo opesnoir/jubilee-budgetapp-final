1ec518c503e2ea4706a460716f52d880094a3902
opesnoir <goodsurroundsme@outlook.com>
2023-07-24 21:48:38 +0200

    feat accountservice methods

--- src/main/java/com/example/jubileebudgetapp/services/AccountService.java
+++ src/main/java/com/example/jubileebudgetapp/services/AccountService.java
@@ -1,13 +1,65 @@
 package com.example.jubileebudgetapp.services;
 
 import com.example.jubileebudgetapp.dtos.AccountDto;
+import com.example.jubileebudgetapp.dtos.UserDto;
+import com.example.jubileebudgetapp.exceptions.AccountIdNotFoundException;
 import com.example.jubileebudgetapp.models.Account;
+import com.example.jubileebudgetapp.repositories.AccountRepository;
+import com.example.jubileebudgetapp.repositories.UserRepository;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class AccountService {
+
+    private final AccountRepository accountRepository;
+
+    private final UserRepository userRepository;
+    private final UserService userService;
+
+    public AccountService(AccountRepository accountRepository, UserRepository userRepository, UserService userService) {
+        this.accountRepository = accountRepository;
+        this.userRepository = userRepository;
+        this.userService = userService;
+    }
+
+    public List<AccountDto> getAccounts(){
+        List<Account> list = accountRepository.findAll();
+        List<AccountDto> collection = new ArrayList<>();
+        for (Account account : list){
+            collection.add(convertAccountToDto(account));
+        }
+        return collection;
+    }
+
+    public AccountDto getAccount(Long id){
+        Account account = accountRepository.findById(id)
+                .orElseThrow(() -> new AccountIdNotFoundException(id));
+
+        return convertAccountToDto(account);
+    }
+
+/*    public UserDto createAccount(){
+
+
+    }*/
+
+    public AccountDto updateAccount(Long id, AccountDto updatedAccountDto){
+        Account existingAccount = accountRepository.findById(id)
+                .orElseThrow(() -> new AccountIdNotFoundException(id));
+
+        updateAccountFromDto(existingAccount, updatedAccountDto);
+        Account updatedAccount = accountRepository.save(existingAccount);
+
+        return convertAccountToDto(updatedAccount);
+    }
+
+    public void deleteAccount(Long id){
+        accountRepository.deleteById(id);
+    }
 
     //helper methods
     public AccountDto convertAccountToDto(Account account){
@@ -20,6 +72,8 @@ public class AccountService {
         if (account.getBalance() != null){
             accountDto.setBalance(account.getBalance());
         }
+        //TODO: vragen of dit klopt
+        userService.convertUserToDto(accountDto.getUser());
         return accountDto;
     }
 
@@ -44,6 +98,5 @@ public class AccountService {
             existingAccount.setLastname(updatedAccountDto.getLastname());
         }
     }
-
 
 }
