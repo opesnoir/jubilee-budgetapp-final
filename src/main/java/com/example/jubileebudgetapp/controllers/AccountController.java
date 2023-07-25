561d23acef0e194f7fdf141050d39dfca640225e
opesnoir <goodsurroundsme@outlook.com>
2023-07-25 08:58:36 +0200

    feat accountcontroller

---
+++ src/main/java/com/example/jubileebudgetapp/controllers/AccountController.java
@@ -0,0 +1,47 @@
+package com.example.jubileebudgetapp.controllers;
+
+import com.example.jubileebudgetapp.dtos.AccountDto;
+import com.example.jubileebudgetapp.services.AccountService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.method.P;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/accounts")
+public class AccountController {
+
+    private final AccountService accountService;
+
+    public AccountController(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<AccountDto>> getAccounts(){
+        return ResponseEntity.ok(accountService.getAccounts());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<AccountDto> getAccount(@PathVariable Long id){
+        return ResponseEntity.ok(accountService.getAccount(id));
+    }
+
+    @PostMapping
+    public AccountDto createAccount(@RequestBody AccountDto accountDto){
+        return accountService.createAccount(accountDto);
+    }
+
+    @RequestMapping(value = "/{id}", method = {RequestMethod.PUT, RequestMethod.PATCH})
+    public ResponseEntity<AccountDto> updateAccount(@PathVariable Long id, @RequestBody AccountDto updatedAccountDto){
+        AccountDto updatedAccount = accountService.updateAccount(id, updatedAccountDto);
+        return ResponseEntity.ok(updatedAccount);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteAccount(@PathVariable Long id){
+        accountService.deleteAccount(id);
+    }
+
+}
