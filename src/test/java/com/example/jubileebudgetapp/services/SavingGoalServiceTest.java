0abd25172c8bb09debeb2b8cc4a528fa0640c64a
opesnoir <goodsurroundsme@outlook.com>
2023-07-27 13:40:08 +0200

    test savinggoalservicetest setup

---
+++ src/test/java/com/example/jubileebudgetapp/services/SavingGoalServiceTest.java
@@ -0,0 +1,44 @@
+package com.example.jubileebudgetapp.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SavingGoalServiceTest {
+
+    @Test
+    void getSavingGoals() {
+    }
+
+    @Test
+    void getSavingGoal() {
+    }
+
+    @Test
+    void createSavingGoal() {
+    }
+
+    @Test
+    void deleteSavingGoal() {
+    }
+
+    @Test
+    void updateSavingGoal() {
+    }
+
+    @Test
+    void convertDtoToSavingGoal() {
+    }
+
+    @Test
+    void convertSavingGoalToDto() {
+    }
+
+    @Test
+    void updateSavingGoalFromDto() {
+    }
+
+    @Test
+    void assignSavingGoalToAccount() {
+    }
+}
